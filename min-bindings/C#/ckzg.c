f5e95c497c8e8ac820408cd08ad6f2ab0dbba9b6
Ramana Kumar <ramana@member.fsf.org>
2022-10-04 19:45:12 +0100

    Start investigating swigless csharp

---
+++ min-bindings/C#/ckzg.c
@@ -0,0 +1,8 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include "c_kzg_4844.h"
+
+uint64_t hello(uint64_t a) {
+  printf("Hello World! %lu\n", a);
+  return 42;
+}
--- min-bindings/C#/tests.cs
+++ min-bindings/C#/tests.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Text;
-using ckzg;
 
-class PoC {
+class ckzg {
+  [global::System.Runtime.InteropServices.DllImport("ckzg.dll", EntryPoint="hello")]
+  public static extern uint hello(uint a);
+  [global::System.Runtime.InteropServices.DllImport("ckzg.dll", EntryPoint="bytes_to_bls_field_wrap")]
+  public static extern void bytes_to_bls_field(byte[] b);
+}
+
+class tests {
   private static void Main(string[] args)
   {
     Console.WriteLine("OK");
+    Console.WriteLine(ckzg.hello(32));
   }
 }
