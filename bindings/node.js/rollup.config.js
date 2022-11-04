7668c4ffa4c89c53f3743e104a590136699b1702
dancoffman <coffman@coinbase.com>
2022-11-03 22:35:09 -0700

    Generate type defs

--- bindings/node.js/rollup.config.js
+++ bindings/node.js/rollup.config.js
@@ -3,7 +3,7 @@ import typescript from "@rollup/plugin-typescript";
 export default {
   input: "kzg.ts",
   output: {
-    file: "dist/index.js",
+    dir: "dist",
     format: "cjs",
   },
   plugins: [typescript()],
