3025c882eab4adfe6142b69e1e7a9228862c1397
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-11-24 17:16:15 +0000

    adding gradle, makefile and tests

--- bindings/java/README.md
+++ bindings/java/README.md
@@ -2,23 +2,17 @@
 
 ## Prerequisites
 
-* Follow the instructions in the [README.md](../../README.md) to install blst and C-KZG. 
+* Follow the instructions in the [README.md](../../README.md) to install blst and build the C-KZG code. 
 * JAVA_HOME environment variable is set to a JDK with an `include` folder containing a jni.h file.
 
-## Windows
-
-```bat
-TBC
+## Build
+```bash
+make c_kzg_4844_jni
 ```
 
-## Linux
+This will install the library in the `src/main/resources/lib` folder according to your os and arch
 
+## Test
 ```bash
-clang -O -Wall -shared -fPIC -I../../blst/bindings -I../../src/ -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -o lib/libckzg4844jni.so c_kzg_4844_jni.c c_kzg_4844.o ../../lib/libblst.a
-```
-
-## Mac-OS
-
-```bash
-TBC
+make test
 ```
---
+++ bindings/java/src/test/java/CKZg4844JNITest.java
@@ -0,0 +1,79 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Random;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CKZg4844JNITest {
+
+  private final Random random = new Random();
+
+  @BeforeEach
+  public void setUp() {
+    CKzg4844JNI.loadTrustedSetup("../../src/trusted_setup.txt");
+  }
+
+  @AfterEach
+  public void cleanUp() {
+    CKzg4844JNI.freeTrustedSetup();
+  }
+
+  @Test
+  public void computesAndVerifiesProofs() {
+
+    final byte[] blob = createRandomBlob();
+    final byte[] blob2 = createRandomBlob();
+
+    assertEquals(blob.length, CKzg4844JNI.BYTES_PER_BLOB);
+    assertEquals(blob2.length, CKzg4844JNI.BYTES_PER_BLOB);
+
+    final byte[] commitment = CKzg4844JNI.blobToKzgCommitment(blob);
+    final byte[] commitment2 = CKzg4844JNI.blobToKzgCommitment(blob2);
+
+    assertEquals(commitment.length, CKzg4844JNI.BYTES_PER_COMMITMENT);
+    assertEquals(commitment2.length, CKzg4844JNI.BYTES_PER_COMMITMENT);
+
+    // flatten blobs and commitments
+    final byte[] blobs = flatten(blob, blob2);
+    final byte[] commitments = flatten(commitment, commitment2);
+
+    final byte[] proof = CKzg4844JNI.computeAggregateKzgProof(blobs, 2);
+
+    assertEquals(proof.length, CKzg4844JNI.BYTES_PER_PROOF);
+
+    assertTrue(CKzg4844JNI.verifyAggregateKzgProof(blobs, commitments, 2, proof));
+
+  }
+
+  @Test
+  public void verifiesPointEvaluationPrecompile() {
+
+    final byte[] commitment = new byte[48];
+    commitment[0] = (byte) 0xc0;
+    final byte[] z = new byte[32];
+    final byte[] y = new byte[32];
+    final byte[] proof = new byte[48];
+    proof[0] = (byte) 0xc0;
+
+    assertTrue(CKzg4844JNI.verifyKzgProof(commitment, z, y, proof));
+
+  }
+
+
+  private byte[] createRandomBlob() {
+    final byte[] blob = new byte[CKzg4844JNI.BYTES_PER_BLOB];
+    random.nextBytes(blob);
+    return blob;
+  }
+
+  private byte[] flatten(byte[]... bytes) {
+    final int capacity = Arrays.stream(bytes).mapToInt(b -> b.length).sum();
+    final ByteBuffer buffer = ByteBuffer.allocate(capacity);
+    Arrays.stream(bytes).forEach(buffer::put);
+    return buffer.array();
+  }
+}
