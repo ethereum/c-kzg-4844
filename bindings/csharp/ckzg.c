7540635de4bf7ed9f67865534c346a5268a6c6fa
ahaifoul <ahaifoul@e1r1p1.1337.ma>
2022-12-18 12:06:56 +0100

    matching_byte_interface

--- bindings/csharp/ckzg.c
+++ bindings/csharp/ckzg.c
@@ -4,6 +4,8 @@
 #include "c_kzg_4844.h"
 #include "ckzg.h"
 
+
+
 KZGSettings* load_trusted_setup_wrap(const char* file) {
   KZGSettings* out = malloc(sizeof(KZGSettings));
 
@@ -24,29 +26,17 @@ void free_trusted_setup_wrap(KZGSettings *s) {
   free(s);
 }
 
-void blob_to_kzg_commitment_wrap(uint8_t out[48], const Blob blob, const KZGSettings *s) {
+C_KZG_RET blob_to_kzg_commitment_wrap(uint8_t out[48], const Blob blob, const KZGSettings *s) {
   KZGCommitment c;
-  blob_to_kzg_commitment(&c, blob, s);
-  bytes_from_g1(out, &c);
+  C_KZG_RET ret = blob_to_kzg_commitment(c, blob, s);
+  return ret;
 }
 
-int verify_aggregate_kzg_proof_wrap(const Blob blobs[], const uint8_t commitments[], size_t n, const uint8_t proof[48], const KZGSettings *s) {
-  KZGProof f;
+int verify_aggregate_kzg_proof_wrap(const Blob blobs[], const KZGCommitment commitments[], size_t n, const uint8_t proof[48], const KZGSettings *s) {
   C_KZG_RET ret;
-  ret = bytes_to_g1(&f, proof);
-  if (ret != C_KZG_OK) return -1;
-
-  KZGCommitment* c = calloc(n, sizeof(KZGCommitment));
-  if (c == NULL) return -2;
-
-  for (size_t i = 0; i < n; i++) {
-    ret = bytes_to_g1(&c[i], &commitments[i * 48]);
-    if (ret != C_KZG_OK) { free(c); return -1; }
-  }
 
   bool b;
-  ret = verify_aggregate_kzg_proof(&b, blobs, c, n, &f, s);
-  free(c);
+  ret = verify_aggregate_kzg_proof(&b, blobs, commitments, n, proof, s);
   if (ret != C_KZG_OK) return -1;
 
   return b ? 0 : 1;
@@ -55,21 +45,14 @@ int verify_aggregate_kzg_proof_wrap(const Blob blobs[], const uint8_t commitment
 C_KZG_RET compute_aggregate_kzg_proof_wrap(uint8_t out[48], const Blob blobs[], size_t n, const KZGSettings *s) {
   KZGProof f;
   C_KZG_RET ret;
-  ret = compute_aggregate_kzg_proof(&f, blobs, n, s);
-  if (ret != C_KZG_OK) return -1;
-  bytes_from_g1(out, &f);
-  return C_KZG_OK;
+  ret = compute_aggregate_kzg_proof(f, blobs, n, s);
+  return ret;
 }
 
 int verify_kzg_proof_wrap(const uint8_t c[48], const uint8_t z[32], const uint8_t y[32], const uint8_t p[48], KZGSettings *s) {
-  KZGCommitment commitment;
-  KZGProof proof;
+
   bool out;
-
-  if (bytes_to_g1(&commitment, c) != C_KZG_OK) return -1;
-  if (bytes_to_g1(&proof, p) != C_KZG_OK) return -1;
-
-  if (verify_kzg_proof(&out, &commitment, z, y, &proof, s) != C_KZG_OK)
+  if (verify_kzg_proof(&out, c, z, y, p, s) != C_KZG_OK)
     return -2;
 
   return out ? 0 : 1;
--- bindings/csharp/ckzg.h
+++ bindings/csharp/ckzg.h
@@ -1,3 +1,7 @@
+#ifndef CKZG_H
+#define CKZG_H
+
+
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,14 +13,22 @@
 #define DLLEXPORT
 #endif
 
+typedef blst_p1 g1_t;         /**< Internal G1 group element type */
+typedef blst_p2 g2_t;         /**< Internal G2 group element type */
+typedef blst_fr fr_t;         /**< Internal Fr field element type */
+
+
+
 DLLEXPORT KZGSettings* load_trusted_setup_wrap(const char* file);
 
 DLLEXPORT void free_trusted_setup_wrap(KZGSettings *s);
 
-DLLEXPORT void blob_to_kzg_commitment_wrap(uint8_t out[48], const Blob blob, const KZGSettings *s);
+DLLEXPORT C_KZG_RET blob_to_kzg_commitment_wrap(uint8_t out[48], const Blob blob, const KZGSettings *s);
 
-DLLEXPORT int verify_aggregate_kzg_proof_wrap(const Blob blobs[], const uint8_t commitments[], size_t n, const uint8_t proof[48], const KZGSettings *s);
+DLLEXPORT int verify_aggregate_kzg_proof_wrap(const Blob blobs[], const KZGCommitment commitments[], size_t n, const uint8_t proof[48], const KZGSettings *s);
 
 DLLEXPORT C_KZG_RET compute_aggregate_kzg_proof_wrap(uint8_t out[48], const Blob blobs[], size_t n, const KZGSettings *s);
 
 DLLEXPORT int verify_kzg_proof_wrap(const uint8_t c[48], const uint8_t z[32], const uint8_t y[32], const uint8_t p[48], KZGSettings *s);
+
+#endif
