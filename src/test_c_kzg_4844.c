b47c1510b32a54b004f4bec75212682513ce4282
Justin Traglia <jtraglia@pm.me>
2023-01-30 21:18:50 +0100

    Use more descriptive test names

--- src/test_c_kzg_4844.c
+++ src/test_c_kzg_4844.c
@@ -79,7 +79,7 @@ static void test_compute_kzg_proof(void) {
     // XXX now verify it!
 }
 
-static void test_blob_to_kzg_commitment__valid_blob(void) {
+static void test_blob_to_kzg_commitment__succeeds_x_less_than_modulus(void) {
     C_KZG_RET ret;
     KZGCommitment c;
     Blob blob;
@@ -105,7 +105,7 @@ static void test_blob_to_kzg_commitment__valid_blob(void) {
     ASSERT_EQUALS(ret, C_KZG_OK);
 }
 
-static void test_blob_to_kzg_commitment__invalid_blob(void) {
+static void test_blob_to_kzg_commitment__fails_x_equal_to_modulus(void) {
     C_KZG_RET ret;
     KZGCommitment c;
     Blob blob;
@@ -131,7 +131,33 @@ static void test_blob_to_kzg_commitment__invalid_blob(void) {
     ASSERT_EQUALS(ret, C_KZG_BADARGS);
 }
 
-static void test_blob_to_kzg_commitment__point_at_infinity(void) {
+static void test_blob_to_kzg_commitment__fails_x_greater_than_modulus(void) {
+    C_KZG_RET ret;
+    KZGCommitment c;
+    Blob blob;
+
+    /*
+     * For a valid field element x < BLS_MODULUS. Therefore x = BLS_MODULUS + 1 should be invalid.
+     * Zero out blob and make the first field element invalid.
+     *
+     * bls_modulus = 52435875175126190479447740508185965837690552500527637822603658699938581184513
+     * x = int(bls_modulus + 1).to_bytes(32, 'little')
+     * print("{" + ", ".join([f"0x{i:02x}" for i in x]) + "}")
+     */
+    uint8_t field_element_bytes[BYTES_PER_FIELD_ELEMENT] = {
+        0x02, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff,
+        0xfe, 0x5b, 0xfe, 0xff, 0x02, 0xa4, 0xbd, 0x53,
+        0x05, 0xd8, 0xa1, 0x09, 0x08, 0xd8, 0x39, 0x33,
+        0x48, 0x7d, 0x9d, 0x29, 0x53, 0xa7, 0xed, 0x73
+    };
+
+    memset(&blob, 0, sizeof(blob));
+    memcpy(blob.bytes, field_element_bytes, BYTES_PER_FIELD_ELEMENT);
+    ret = blob_to_kzg_commitment(&c, &blob, &s);
+    ASSERT_EQUALS(ret, C_KZG_BADARGS);
+}
+
+static void test_blob_to_kzg_commitment__succeeds_point_at_infinity(void) {
     C_KZG_RET ret;
     KZGCommitment c;
     Blob blob;
@@ -162,9 +188,10 @@ int main(void)
 {
     setup();
     RUN(test_compute_kzg_proof);
-    RUN(test_blob_to_kzg_commitment__valid_blob);
-    RUN(test_blob_to_kzg_commitment__invalid_blob);
-    RUN(test_blob_to_kzg_commitment__point_at_infinity);
+    RUN(test_blob_to_kzg_commitment__succeeds_x_less_than_modulus);
+    RUN(test_blob_to_kzg_commitment__fails_x_equal_to_modulus);
+    RUN(test_blob_to_kzg_commitment__fails_x_greater_than_modulus);
+    RUN(test_blob_to_kzg_commitment__succeeds_point_at_infinity);
     teardown();
 
     return TEST_REPORT();
