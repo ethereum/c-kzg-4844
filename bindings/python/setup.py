50211b71730d5d3d690a7b4dbe9d3a17327fd704
Justin Traglia <jtraglia@pm.me>
2023-02-06 16:19:16 -0600

    Add src dir to include dirs

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -9,10 +9,10 @@ def main():
             Extension(
                 "ckzg",
                 sources=["ckzg.c", "../../src/c_kzg_4844.c"],
-                include_dirs=['../../inc'],
-                define_macros=[('FIELD_ELEMENTS_PER_BLOB', '4096')],
-                library_dirs=['../../lib'],
-                libraries=['blst'])])
+                include_dirs=["../../inc", "../../src"],
+                define_macros=[("FIELD_ELEMENTS_PER_BLOB", "4096")],
+                library_dirs=["../../lib"],
+                libraries=["blst"])])
 
 if __name__ == "__main__":
     main()
