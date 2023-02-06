06ef193142e145e79c5ecb885ecca56e9007971e
Justin Traglia <jtraglia@pm.me>
2023-02-06 16:11:31 -0600

    Fix python bindings on macOS

---
+++ bindings/python/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup, Extension
+
+def main():
+    setup(
+        name="ckzg",
+        version="1.0.0",
+        description="Python interface for C-KZG-4844",
+        ext_modules=[
+            Extension(
+                "ckzg",
+                sources=["ckzg.c", "../../src/c_kzg_4844.c"],
+                include_dirs=['../../inc'],
+                define_macros=[('FIELD_ELEMENTS_PER_BLOB', '4096')],
+                library_dirs=['../../lib'],
+                libraries=['blst'])])
+
+if __name__ == "__main__":
+    main()
