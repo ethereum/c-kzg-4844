68a14811482b145ae76ccc082da1b109d884b6e0
Justin Traglia <jtraglia@pm.me>
2023-02-13 11:04:50 -0600

    Disable compiler optimizations for tests

--- src/test_c_kzg_4844.c
+++ src/test_c_kzg_4844.c
@@ -105,6 +105,15 @@ static void test_c_kzg_malloc__fails_size_equal_to_zero(void) {
     ASSERT_EQUALS(ptr, NULL);
 }
 
+static void test_c_kzg_malloc__fails_too_big(void) {
+    C_KZG_RET ret;
+    void *ptr = NULL;
+
+    ret = c_kzg_malloc(&ptr, UINT64_MAX);
+    ASSERT_EQUALS(ret, C_KZG_MALLOC);
+    ASSERT_EQUALS(ptr, NULL);
+}
+
 static void test_c_kzg_calloc__succeeds_size_greater_than_zero(void) {
     C_KZG_RET ret;
     void *ptr = NULL;
@@ -130,6 +139,15 @@ static void test_c_kzg_calloc__fails_size_equal_to_zero(void) {
 
     ret = c_kzg_calloc(&ptr, 123, 0);
     ASSERT_EQUALS(ret, C_KZG_BADARGS);
+    ASSERT_EQUALS(ptr, NULL);
+}
+
+static void test_c_kzg_calloc__fails_too_big(void) {
+    C_KZG_RET ret;
+    void *ptr = NULL;
+
+    ret = c_kzg_calloc(&ptr, UINT64_MAX, UINT64_MAX);
+    ASSERT_EQUALS(ret, C_KZG_MALLOC);
     ASSERT_EQUALS(ptr, NULL);
 }
 
@@ -760,9 +778,11 @@ int main(void) {
     setup();
     RUN(test_c_kzg_malloc__succeeds_size_greater_than_zero);
     RUN(test_c_kzg_malloc__fails_size_equal_to_zero);
+    RUN(test_c_kzg_malloc__fails_too_big);
     RUN(test_c_kzg_calloc__succeeds_size_greater_than_zero);
     RUN(test_c_kzg_calloc__fails_size_equal_to_zero);
     RUN(test_c_kzg_calloc__fails_count_equal_to_zero);
+    RUN(test_c_kzg_calloc__fails_too_big);
     RUN(test_blob_to_kzg_commitment__succeeds_x_less_than_modulus);
     RUN(test_blob_to_kzg_commitment__fails_x_equal_to_modulus);
     RUN(test_blob_to_kzg_commitment__fails_x_greater_than_modulus);
