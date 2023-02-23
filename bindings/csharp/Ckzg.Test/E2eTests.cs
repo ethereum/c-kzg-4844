86673bfd6c201d75a9e5010d072742aac77c8176
Justin Traglia <jtraglia@pm.me>
2023-02-22 20:03:44 -0600

    Update csharp bindings

--- bindings/csharp/Ckzg.Test/E2eTests.cs
+++ bindings/csharp/Ckzg.Test/E2eTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.IO;
+using System.Text.Json;
 
 namespace Ckzg.Test;
 
@@ -16,7 +17,11 @@ public class BasicKzgTests
     string VerifyBlobKzgProofTests = Path.Join(TestDir, "verify_blob_kzg_proof");
     string VerifyBlobKzgProofBatchTests = Path.Join(TestDir, "verify_blob_kzg_proof_batch");
 
-    public static byte[] StringToByteArray(string hex)
+    ///////////////////////////////////////////////////////////////////////////
+    // Helper Functions
+    ///////////////////////////////////////////////////////////////////////////
+
+    public static byte[] GetBytes(string hex)
     {
         return Enumerable.Range(0, hex.Length)
                          .Where(x => x % 2 == 0)
@@ -24,25 +29,17 @@ public class BasicKzgTests
                          .ToArray();
     }
 
-    public static byte[] GetBytes(String path)
+    public static byte[] GetFlatBytes(List<string> strings)
     {
-        string hex = System.IO.File.ReadAllText(path);
-        return StringToByteArray(hex);
-    }
-
-    public static byte[] GetFlatBytes(String path)
-    {
-        List<String> files = Directory.GetFiles(path).ToList();
-        files.Sort();
-        List<byte[]> filesBytes = new List<byte[]>();
-        foreach (String file in files)
+        List<byte[]> stringBytes = new List<byte[]>();
+        foreach (string str in strings)
         {
-            filesBytes.Add(GetBytes(file));
+            stringBytes.Add(GetBytes(str));
         }
 
-        byte[] flatBytes = new byte[filesBytes.Sum(b => b.Length)];
+        byte[] flatBytes = new byte[stringBytes.Sum(b => b.Length)];
         int offset = 0;
-        foreach (byte[] bytes in filesBytes)
+        foreach (byte[] bytes in stringBytes)
         {
             System.Buffer.BlockCopy(bytes, 0, flatBytes, offset, bytes.Length);
             offset += bytes.Length;
@@ -51,10 +48,9 @@ public class BasicKzgTests
         return flatBytes;
     }
 
-    public static bool GetBoolean(String path)
-    {
-        return System.IO.File.ReadAllText(path).Contains("true");
-    }
+    ///////////////////////////////////////////////////////////////////////////
+    // Testing Setup/Teardown
+    ///////////////////////////////////////////////////////////////////////////
 
     [SetUp]
     public void Setup()
@@ -69,148 +65,304 @@ public class BasicKzgTests
         Ckzg.FreeTrustedSetup(ts);
     }
 
+    ///////////////////////////////////////////////////////////////////////////
+    // BlobToKzgCommitment
+    ///////////////////////////////////////////////////////////////////////////
+
+    public class BlobToKzgCommitmentInput
+    {
+        public string blob { get; set; } = null!;
+    }
+
+    public class BlobToKzgCommitmentOutput
+    {
+        public string? commitment { get; set; } = null!;
+    }
+
+    public class BlobToKzgCommitmentTest
+    {
+        public BlobToKzgCommitmentInput input { get; set; } = null!;
+        public BlobToKzgCommitmentOutput output { get; set; } = null!;
+    }
+
     [TestCase]
     public unsafe void TestBlobToKzgCommitment()
     {
-        foreach (String test in Directory.GetDirectories(BlobToKZGCommitmentTests))
+
+        foreach (String testFile in Directory.GetFiles(BlobToKZGCommitmentTests))
         {
+            String? json = File.ReadAllText(testFile);
+            BlobToKzgCommitmentTest? test = JsonSerializer.Deserialize<BlobToKzgCommitmentTest>(json);
+
             byte[] commitment = new byte[48];
-            byte[] blob = GetBytes(Path.Join(test, "blob.txt"));
+            byte[] blob = GetBytes(test!.input!.blob);
+
             fixed (byte *pCommitment = commitment, pBlob = blob)
             {
                 Ckzg.Ret ret = Ckzg.BlobToKzgCommitment(pCommitment, pBlob, ts);
                 if (ret == Ckzg.Ret.Ok)
                 {
-                    byte[] expectedCommitment = GetBytes(Path.Join(test, "commitment.txt"));
+                    string? commitmentStr = test!.output!.commitment;
+                    Assert.That(commitmentStr, Is.Not.EqualTo(null));
+                    byte[] expectedCommitment = GetBytes(commitmentStr);
                     Assert.That(commitment, Is.EqualTo(expectedCommitment));
                 }
                 else
                 {
-                    Assert.False(System.IO.File.Exists(Path.Join(test, "commitment.txt")));
+                    Assert.That(test.output.commitment, Is.EqualTo(null));
                 }
             }
         }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // ComputeKzgProof
+    ///////////////////////////////////////////////////////////////////////////
+
+    public class ComputeKzgProofInput
+    {
+        public string blob { get; set; } = null!;
+        public string input_point { get; set; } = null!;
+    }
+
+    public class ComputeKzgProofOutput
+    {
+        public string? proof { get; set; } = null!;
+    }
+
+    public class ComputeKzgProofTest
+    {
+        public ComputeKzgProofInput input { get; set; } = null!;
+        public ComputeKzgProofOutput output { get; set; } = null!;
     }
 
     [TestCase]
     public unsafe void TestComputeKzgProof()
     {
-        foreach (String test in Directory.GetDirectories(ComputeKzgProofTests))
+        foreach (String testFile in Directory.GetFiles(ComputeKzgProofTests))
         {
+            String? json = File.ReadAllText(testFile);
+            ComputeKzgProofTest? test = JsonSerializer.Deserialize<ComputeKzgProofTest>(json);
+
             byte[] proof = new byte[48];
-            byte[] blob = GetBytes(Path.Join(test, "blob.txt"));
-            byte[] inputPoint = GetBytes(Path.Join(test, "input_point.txt"));
+            byte[] blob = GetBytes(test!.input!.blob);
+            byte[] inputPoint = GetBytes(test!.input!.input_point);
+
             fixed (byte *pProof = proof, pBlob = blob, pInputPoint = inputPoint)
             {
                 Ckzg.Ret ret = Ckzg.ComputeKzgProof(pProof, pBlob, pInputPoint, ts);
                 if (ret == Ckzg.Ret.Ok)
                 {
-                    byte[] expectedProof = GetBytes(Path.Join(test, "proof.txt"));
+                    string? proofStr = test!.output!.proof;
+                    Assert.That(proofStr, Is.Not.EqualTo(null));
+                    byte[] expectedProof = GetBytes(proofStr);
                     Assert.That(proof, Is.EqualTo(expectedProof));
                 }
                 else
                 {
-                    Assert.False(System.IO.File.Exists(Path.Join(test, "proof.txt")));
+                    Assert.That(test.output.proof, Is.EqualTo(null));
                 }
             }
         }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // ComputeBlobKzgProof
+    ///////////////////////////////////////////////////////////////////////////
+
+    public class ComputeBlobKzgProofInput
+    {
+        public string blob { get; set; } = null!;
+    }
+
+    public class ComputeBlobKzgProofOutput
+    {
+        public string? proof { get; set; } = null!;
+    }
+
+    public class ComputeBlobKzgProofTest
+    {
+        public ComputeBlobKzgProofInput input { get; set; } = null!;
+        public ComputeBlobKzgProofOutput output { get; set; } = null!;
     }
 
     [TestCase]
     public unsafe void TestComputeBlobKzgProof()
     {
-        foreach (String test in Directory.GetDirectories(ComputeBlobKzgProofTests))
+        foreach (String testFile in Directory.GetFiles(ComputeBlobKzgProofTests))
         {
+            String? json = File.ReadAllText(testFile);
+            ComputeBlobKzgProofTest? test = JsonSerializer.Deserialize<ComputeBlobKzgProofTest>(json);
+
             byte[] proof = new byte[48];
-            byte[] blob = GetBytes(Path.Join(test, "blob.txt"));
+            byte[] blob = GetBytes(test!.input!.blob);
+
             fixed (byte *pProof = proof, pBlob = blob)
             {
                 Ckzg.Ret ret = Ckzg.ComputeBlobKzgProof(pProof, pBlob, ts);
                 if (ret == Ckzg.Ret.Ok)
                 {
-                    byte[] expectedProof = GetBytes(Path.Join(test, "proof.txt"));
+                    string? proofStr = test!.output!.proof;
+                    Assert.That(proofStr, Is.Not.EqualTo(null));
+                    byte[] expectedProof = GetBytes(proofStr);
                     Assert.That(proof, Is.EqualTo(expectedProof));
                 }
                 else
                 {
-                    Assert.False(System.IO.File.Exists(Path.Join(test, "proof.txt")));
+                    Assert.That(test.output.proof, Is.EqualTo(null));
                 }
             }
         }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // VerifyKzgProof
+    ///////////////////////////////////////////////////////////////////////////
+
+    public class VerifyKzgProofInput
+    {
+        public string commitment { get; set; } = null!;
+        public string input_point { get; set; } = null!;
+        public string claimed_value { get; set; } = null!;
+        public string proof { get; set; } = null!;
+    }
+
+    public class VerifyKzgProofOutput
+    {
+        public bool? valid { get; set; } = null!;
+    }
+
+    public class VerifyKzgProofTest
+    {
+        public VerifyKzgProofInput input { get; set; } = null!;
+        public VerifyKzgProofOutput output { get; set; } = null!;
     }
 
     [TestCase]
     public unsafe void TestVerifyKzgProof()
     {
-        foreach (String test in Directory.GetDirectories(VerifyKzgProofTests))
+        foreach (String testFile in Directory.GetFiles(VerifyKzgProofTests))
         {
-            bool ok = false;
-            byte[] commitment = GetBytes(Path.Join(test, "commitment.txt"));
-            byte[] inputPoint = GetBytes(Path.Join(test, "input_point.txt"));
-            byte[] claimedValue = GetBytes(Path.Join(test, "claimed_value.txt"));
-            byte[] proof = GetBytes(Path.Join(test, "proof.txt"));
+            String? json = File.ReadAllText(testFile);
+            VerifyKzgProofTest? test = JsonSerializer.Deserialize<VerifyKzgProofTest>(json);
+
+            bool valid = false;
+            byte[] commitment = GetBytes(test!.input!.commitment);
+            byte[] inputPoint = GetBytes(test!.input!.input_point);
+            byte[] claimedValue = GetBytes(test!.input!.claimed_value);
+            byte[] proof = GetBytes(test!.input!.proof);
+
             fixed (byte *pCommitment = commitment, pInputPoint = inputPoint, pClaimedValue = claimedValue, pProof = proof)
             {
-                Ckzg.Ret ret = Ckzg.VerifyKzgProof(&ok, pCommitment, pInputPoint, pClaimedValue, pProof, ts);
+                Ckzg.Ret ret = Ckzg.VerifyKzgProof(&valid, pCommitment, pInputPoint, pClaimedValue, pProof, ts);
                 if (ret == Ckzg.Ret.Ok)
                 {
-                    bool expectedOk = GetBoolean(Path.Join(test, "ok.txt"));
-                    Assert.That(ok, Is.EqualTo(expectedOk));
+                    Assert.That(valid, Is.EqualTo(test.output.valid));
                 }
                 else
                 {
-                    Assert.False(System.IO.File.Exists(Path.Join(test, "ok.txt")));
+                    Assert.That(test.output.valid, Is.EqualTo(null));
                 }
             }
         }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // VerifyBlobKzgProof
+    ///////////////////////////////////////////////////////////////////////////
+
+    public class VerifyBlobKzgProofInput
+    {
+        public string blob { get; set; } = null!;
+        public string commitment { get; set; } = null!;
+        public string proof { get; set; } = null!;
+    }
+
+    public class VerifyBlobKzgProofOutput
+    {
+        public bool? valid { get; set; } = null!;
+    }
+
+    public class VerifyBlobKzgProofTest
+    {
+        public VerifyBlobKzgProofInput input { get; set; } = null!;
+        public VerifyBlobKzgProofOutput output { get; set; } = null!;
     }
 
     [TestCase]
     public unsafe void TestVerifyBlobKzgProof()
     {
-        foreach (String test in Directory.GetDirectories(VerifyBlobKzgProofTests))
+        foreach (String testFile in Directory.GetFiles(VerifyBlobKzgProofTests))
         {
-            bool ok = false;
-            byte[] blob = GetBytes(Path.Join(test, "blob.txt"));
-            byte[] commitment = GetBytes(Path.Join(test, "commitment.txt"));
-            byte[] proof = GetBytes(Path.Join(test, "proof.txt"));
+            String? json = File.ReadAllText(testFile);
+            VerifyBlobKzgProofTest? test = JsonSerializer.Deserialize<VerifyBlobKzgProofTest>(json);
+
+            bool valid = false;
+            byte[] blob = GetBytes(test!.input!.blob);
+            byte[] commitment = GetBytes(test!.input!.commitment);
+            byte[] proof = GetBytes(test!.input!.proof);
+
             fixed (byte *pBlob = blob, pCommitment = commitment, pProof = proof)
             {
-                Ckzg.Ret ret = Ckzg.VerifyBlobKzgProof(&ok, pBlob, pCommitment, pProof, ts);
+                Ckzg.Ret ret = Ckzg.VerifyBlobKzgProof(&valid, pBlob, pCommitment, pProof, ts);
                 if (ret == Ckzg.Ret.Ok)
                 {
-                    bool expectedOk = GetBoolean(Path.Join(test, "ok.txt"));
-                    Assert.That(ok, Is.EqualTo(expectedOk));
+                    Assert.That(valid, Is.EqualTo(test.output.valid));
                 }
                 else
                 {
-                    Assert.False(System.IO.File.Exists(Path.Join(test, "ok.txt")));
+                    Assert.That(test.output.valid, Is.EqualTo(null));
                 }
             }
         }
     }
 
+    ///////////////////////////////////////////////////////////////////////////
+    // VerifyBlobKzgProofBatch
+    ///////////////////////////////////////////////////////////////////////////
+
+    public class VerifyBlobKzgProofBatchInput
+    {
+        public List<string> blobs { get; set; } = null!;
+        public List<string> commitments { get; set; } = null!;
+        public List<string> proofs { get; set; } = null!;
+    }
+
+    public class VerifyBlobKzgProofBatchOutput
+    {
+        public bool? valid { get; set; } = null!;
+    }
+
+    public class VerifyBlobKzgProofBatchTest
+    {
+        public VerifyBlobKzgProofBatchInput input { get; set; } = null!;
+        public VerifyBlobKzgProofBatchOutput output { get; set; } = null!;
+    }
+
     [TestCase]
     public unsafe void TestVerifyBlobKzgProofBatch()
     {
-        foreach (String test in Directory.GetDirectories(VerifyBlobKzgProofBatchTests))
+        foreach (String testFile in Directory.GetFiles(VerifyBlobKzgProofBatchTests))
         {
-            bool ok = false;
-            byte[] blobs = GetFlatBytes(Path.Join(test, "blobs"));
-            byte[] commitments = GetFlatBytes(Path.Join(test, "commitments"));
-            byte[] proofs = GetFlatBytes(Path.Join(test, "proofs"));
+            String? json = File.ReadAllText(testFile);
+            VerifyBlobKzgProofBatchTest? test = JsonSerializer.Deserialize<VerifyBlobKzgProofBatchTest>(json);
+
+            bool valid = false;
+            byte[] blobs = GetFlatBytes(test!.input!.blobs);
+            byte[] commitments = GetFlatBytes(test!.input!.commitments);
+            byte[] proofs = GetFlatBytes(test!.input!.proofs);
             int count = blobs.Length / Ckzg.BytesPerBlob;
+
             fixed (byte *pBlobs = blobs, pCommitments = commitments, pProofs = proofs)
             {
-                Ckzg.Ret ret = Ckzg.VerifyBlobKzgProofBatch(&ok, pBlobs, pCommitments, pProofs, count, ts);
+                Ckzg.Ret ret = Ckzg.VerifyBlobKzgProofBatch(&valid, pBlobs, pCommitments, pProofs, count, ts);
                 if (ret == Ckzg.Ret.Ok)
                 {
-                    bool expectedOk = GetBoolean(Path.Join(test, "ok.txt"));
-                    Assert.That(ok, Is.EqualTo(expectedOk));
+                    Assert.That(valid, Is.EqualTo(test!.output!.valid));
                 }
                 else
                 {
-                    Assert.False(System.IO.File.Exists(Path.Join(test, "ok.txt")));
+                    Assert.That(test.output.valid, Is.EqualTo(null));
                 }
             }
         }
