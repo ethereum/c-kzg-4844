ac9af0cfb9f8e7a5d98950cbb7b807ec5d41da39
matthewkeil <me@matthewkeil.com>
2023-02-26 05:02:21 -0600

    refactor(nodejs-bindings): get_bytes

--- bindings/node.js/src/functions.cc
+++ bindings/node.js/src/functions.cc
@@ -1,40 +1,12 @@
 #include "functions.h"
 
-uint8_t *get_bytes_from_info(
-    const Napi::CallbackInfo &info,
+uint8_t *get_bytes(
     const Napi::Env &env,
     const KzgBindings *bindings,
-    uint32_t arg_position,
+    Napi::Value val,
     size_t length,
     std::string_view name)
 {
-    if (!(info[arg_position].IsTypedArray() && info[arg_position].As<Napi::TypedArray>().TypedArrayType() == napi_uint8_array))
-    {
-        std::ostringstream msg;
-        msg << name << " must be a Uint8Array";
-        Napi::TypeError::New(env, msg.str()).ThrowAsJavaScriptException();
-        return nullptr;
-    }
-    Napi::Uint8Array array = info[arg_position].As<Napi::Uint8Array>();
-    if (array.ByteLength() != length)
-    {
-        std::ostringstream msg;
-        msg << name << " must be " << length << " bytes long";
-        Napi::TypeError::New(env, msg.str()).ThrowAsJavaScriptException();
-        return nullptr;
-    }
-    return array.Data();
-}
-
-uint8_t *get_bytes_from_array(
-    const Napi::Array &js_array,
-    const Napi::Env &env,
-    const KzgBindings *bindings,
-    uint32_t arg_position,
-    size_t length,
-    std::string_view name)
-{
-    Napi::Value val = js_array[arg_position];
     if (!(val.IsTypedArray() && val.As<Napi::TypedArray>().TypedArrayType() == napi_uint8_array))
     {
         std::ostringstream msg;
@@ -84,7 +56,7 @@ Napi::Value BlobToKzgCommitment(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     KzgBindings *bindings = static_cast<KzgBindings *>(info.Data());
-    uint8_t *blob = get_bytes_from_info(info, env, bindings, 0, bindings->_global_state->_bytes_per_blob, "blob");
+    uint8_t *blob = get_bytes(env, bindings, info[0], bindings->_global_state->_bytes_per_blob, "blob");
     if (blob == nullptr || env.IsExceptionPending())
     {
         return env.Undefined();
@@ -103,12 +75,12 @@ Napi::Value ComputeKzgProof(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     KzgBindings *bindings = static_cast<KzgBindings *>(info.Data());
-    uint8_t *blob = get_bytes_from_info(info, env, bindings, 0, bindings->_global_state->_bytes_per_blob, "blob");
+    uint8_t *blob = get_bytes(env, bindings, info[0], bindings->_global_state->_bytes_per_blob, "blob");
     if (blob == nullptr || env.IsExceptionPending())
     {
         return env.Undefined();
     }
-    uint8_t *z_bytes = get_bytes_from_info(info, env, bindings, 1, 32, "zBytes");
+    uint8_t *z_bytes = get_bytes(env, bindings, info[1], 32, "zBytes");
     if (blob == nullptr || env.IsExceptionPending())
     {
         return env.Undefined();
@@ -134,7 +106,7 @@ Napi::Value ComputeBlobKzgProof(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     KzgBindings *bindings = static_cast<KzgBindings *>(info.Data());
-    uint8_t *blob = get_bytes_from_info(info, env, bindings, 0, bindings->_global_state->_bytes_per_blob, "blob");
+    uint8_t *blob = get_bytes(env, bindings, info[0], bindings->_global_state->_bytes_per_blob, "blob");
     if (blob == nullptr || env.IsExceptionPending())
     {
         return env.Undefined();
@@ -160,10 +132,10 @@ Napi::Value VerifyKzgProof(const Napi::CallbackInfo &info)
     Napi::Env env = info.Env();
     KzgBindings *bindings = static_cast<KzgBindings *>(info.Data());
 
-    uint8_t *commitment_bytes = get_bytes_from_info(info, env, bindings, 0, bindings->_global_state->_bytes_per_commitment, "commitmentBytes");
-    uint8_t *z_bytes = get_bytes_from_info(info, env, bindings, 1, 32, "zBytes");
-    uint8_t *y_bytes = get_bytes_from_info(info, env, bindings, 2, 32, "yBytes");
-    uint8_t *proof_bytes = get_bytes_from_info(info, env, bindings, 3, bindings->_global_state->_bytes_per_proof, "proofBytes");
+    uint8_t *commitment_bytes = get_bytes(env, bindings, info[0], bindings->_global_state->_bytes_per_commitment, "commitmentBytes");
+    uint8_t *z_bytes = get_bytes(env, bindings, info[1], 32, "zBytes");
+    uint8_t *y_bytes = get_bytes(env, bindings, info[2], 32, "yBytes");
+    uint8_t *proof_bytes = get_bytes(env, bindings, info[3], bindings->_global_state->_bytes_per_proof, "proofBytes");
     bool out;
     C_KZG_RET ret = verify_kzg_proof(
         &out,
@@ -184,9 +156,9 @@ Napi::Value VerifyBlobKzgProof(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     KzgBindings *bindings = static_cast<KzgBindings *>(info.Data());
-    uint8_t *blob_bytes = get_bytes_from_info(info, env, bindings, 0, bindings->_global_state->_bytes_per_blob, "blob");
-    uint8_t *commitment_bytes = get_bytes_from_info(info, env, bindings, 1, bindings->_global_state->_bytes_per_commitment, "commitmentBytes");
-    uint8_t *proof_bytes = get_bytes_from_info(info, env, bindings, 2, bindings->_global_state->_bytes_per_proof, "proofBytes");
+    uint8_t *blob_bytes = get_bytes(env, bindings, info[0], bindings->_global_state->_bytes_per_blob, "blob");
+    uint8_t *commitment_bytes = get_bytes(env, bindings, info[1], bindings->_global_state->_bytes_per_commitment, "commitmentBytes");
+    uint8_t *proof_bytes = get_bytes(env, bindings, info[2], bindings->_global_state->_bytes_per_proof, "proofBytes");
     bool out;
     C_KZG_RET ret = verify_blob_kzg_proof(
         &out,
@@ -214,32 +186,45 @@ Napi::Value VerifyBlobKzgProofBatch(const Napi::CallbackInfo &info)
     Napi::Array blobs_array = info[0].As<Napi::Array>();
     Napi::Array commitments_array = info[1].As<Napi::Array>();
     Napi::Array proofs_array = info[2].As<Napi::Array>();
-    uint32_t blobs_count = blobs_array.Length();
-    uint32_t commitments_count = commitments_array.Length();
-    uint32_t proofs_count = proofs_array.Length();
-    if (!(blobs_count == commitments_count == proofs_count))
+    uint32_t count = blobs_array.Length();
+    if (!(count == commitments_array.Length() == proofs_array.Length()))
     {
         Napi::Error::New(env, "blobs, commitments, and proofs arrays must be the same length").ThrowAsJavaScriptException();
         return env.Undefined();
     }
-    Blob blobs[blobs_count];
-    Bytes48 commitments[blobs_count];
-    Bytes48 proofs[blobs_count];
-    for (uint32_t index = 0; index < blobs_count; index++)
+    Blob blobs[count];
+    Bytes48 commitments[count];
+    Bytes48 proofs[count];
+    for (uint32_t index = 0; index < count; index++)
     {
-        uint8_t *blob = get_bytes_from_array(blobs_array, env, bindings, 0, bindings->_global_state->_bytes_per_blob, "blob");
+        uint8_t *blob = get_bytes(
+            env,
+            bindings,
+            blobs_array[index],
+            bindings->_global_state->_bytes_per_blob,
+            "blob");
         if (blob == nullptr || env.IsExceptionPending())
         {
             return env.Undefined();
         }
         memcpy(&blobs[index], blob, bindings->_global_state->_bytes_per_blob);
-        uint8_t *commitment = get_bytes_from_array(commitments_array, env, bindings, 1, bindings->_global_state->_bytes_per_commitment, "commitmentBytes");
+        uint8_t *commitment = get_bytes(
+            env,
+            bindings,
+            commitments_array[index],
+            bindings->_global_state->_bytes_per_commitment,
+            "commitment");
         if (commitment == nullptr || env.IsExceptionPending())
         {
             return env.Undefined();
         }
         memcpy(&commitments[index], commitment, bindings->_global_state->_bytes_per_commitment);
-        uint8_t *proof = get_bytes_from_array(proofs_array, env, bindings, 2, bindings->_global_state->_bytes_per_proof, "proofBytes");
+        uint8_t *proof = get_bytes(
+            env,
+            bindings,
+            proofs_array[index],
+            bindings->_global_state->_bytes_per_proof,
+            "proof");
         if (proof == nullptr || env.IsExceptionPending())
         {
             return env.Undefined();
@@ -252,7 +237,7 @@ Napi::Value VerifyBlobKzgProofBatch(const Napi::CallbackInfo &info)
         blobs,
         commitments,
         proofs,
-        blobs_count,
+        count,
         bindings->_settings.get());
     if (ret != C_KZG_OK)
     {
