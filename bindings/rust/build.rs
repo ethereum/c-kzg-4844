f384175810d77b09ba81347c03c7302f15f245c6
Divma <26765164+divagant-martian@users.noreply.github.com>
2023-03-27 09:47:24 -0500

    Reproducible rust bindings (#243)
    * modify directory structure to isolate the generated bindings
    
    * add a lib.rs file
    
    * move deref impls to the extension file
    
    * remove unused types from generated bindings
    
    * cleanup new lines to reduce diff noise
    
    * reorder definitions to reduce diff noise
    
    * move sync and send impls to the extension file
    
    * generate bindings
    
    * blacklist used bindings; create snapshots dir to ensure freshness in the future
    
    * fix typo. Ty @pawanjay176
    
    * run cargo build after merge
    
    * custom impl for KZGCommitment and KZGProof
    
    * final touches

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -14,7 +14,8 @@ fn move_file(src: &Path, dst: &Path) -> Result<(), String> {
 }
 
 fn main() {
-    let root_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("../../");
+    let cargo_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let root_dir = cargo_dir.join("../../");
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     // Ensure libblst exists in `OUT_DIR`
@@ -72,20 +73,121 @@ fn main() {
     println!("cargo:rustc-link-lib=static=ckzg");
     println!("cargo:rustc-link-lib=static=blst");
 
-    // Write the compile time variable to a consts.rs file to be imported to the bindings module.
-    let const_file = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("src/consts.rs");
-    std::fs::write(
-        const_file,
-        format!(
-            "pub const FIELD_ELEMENTS_PER_BLOB: usize = {};",
-            field_elements_per_blob
-        ),
-    )
-    .unwrap();
+    let bindings_out_path = cargo_dir.join("src/bindings/generated.rs");
+    let build_target = env::var("TARGET").unwrap();
+    let snapshot_path = cargo_dir.join("snapshots").join(format!(
+        "bindings_{build_target}_{field_elements_per_blob}.rs"
+    ));
+    make_bindings(
+        field_elements_per_blob,
+        &root_dir,
+        bindings_out_path,
+        snapshot_path,
+    );
 
     // Cleanup
     let obj_file = root_dir.join("src/c_kzg_4844.o");
     if obj_file.exists() {
         std::fs::remove_file(obj_file).unwrap();
     }
+}
+
+fn make_bindings<P>(
+    field_elements_per_blob: usize,
+    root_dir: &Path,
+    bindings_out_path: P,
+    snapshot_path: P,
+) where
+    P: AsRef<std::path::Path>,
+{
+    use bindgen::Builder;
+
+    #[derive(Debug)]
+    struct Callbacks;
+    impl bindgen::callbacks::ParseCallbacks for Callbacks {
+        fn int_macro(&self, name: &str, _value: i64) -> Option<bindgen::callbacks::IntKind> {
+            match name {
+                "FIELD_ELEMENTS_PER_BLOB"
+                | "BYTES_PER_COMMITMENT"
+                | "BYTES_PER_PROOF"
+                | "BYTES_PER_FIELD_ELEMENT"
+                | "BYTES_PER_BLOB" => Some(bindgen::callbacks::IntKind::Custom {
+                    name: "usize",
+                    is_signed: false,
+                }),
+                _ => None,
+            }
+        }
+    }
+
+    let header_file_path = root_dir.join("src/c_kzg_4844.h");
+    let header_file = header_file_path.to_str().expect("valid header file");
+
+    let inc_dir_path = root_dir.join("inc");
+    let inc_dir = inc_dir_path.to_str().expect("valid inc dir");
+
+    let bindings = Builder::default()
+        /*
+         * Header definitions.
+         */
+        // Inject the constant as C code so that the C compiler can use it.
+        // -D is not supported by bindgen https://github.com/rust-lang/rust-bindgen/issues/2394
+        .header_contents(
+            "consts",
+            &format!("#define FIELD_ELEMENTS_PER_BLOB {field_elements_per_blob}"),
+        )
+        .header(header_file)
+        .clang_args([format!("-I{inc_dir}")])
+        // Since this is not part of the header file, needs to be allowed explicitly.
+        .allowlist_var("FIELD_ELEMENTS_PER_BLOB")
+        // Get bindings only for the header file.
+        .allowlist_file(".*/c_kzg_4844.h")
+        /*
+         * Cleanup instructions.
+         */
+        // Remove stdio definitions related to FILE.
+        .opaque_type("FILE")
+        // Remove the definition of FILE to use the libc one, which is more convenient.
+        .blocklist_type("FILE")
+        // Inject rust code using libc's FILE
+        .raw_line("use libc::FILE;")
+        // Do no generate layout tests.
+        .layout_tests(false)
+        // Extern functions do not need individual extern blocks.
+        .merge_extern_blocks(true)
+        // We implement Drop for this type. Copy is not allowed for types with destructors.
+        .no_copy("KZGSettings")
+        /*
+         * API improvements.
+         */
+        // Do not create individual constants for enum variants.
+        .rustified_enum("C_KZG_RET")
+        // Make constants used as sizes `usize`.
+        .parse_callbacks(Box::new(Callbacks))
+        // Add PartialEq and Eq impls to types.
+        .derive_eq(true)
+        // Blobs are big, we don't want rust to liberally copy them around.
+        .no_copy("Blob")
+        // Do not make fields public. If we want to modify them we can create setters/mutable
+        // getters when necessary.
+        .default_visibility(bindgen::FieldVisibilityKind::Private)
+        // Blocklist this type alias to use a custom implementation. If this stops being a type
+        // alias this line needs to be removed.
+        .blocklist_type("KZGCommitment")
+        // Blocklist this type alias to use a custom implementation. If this stops being a type
+        // alias this line needs to be removed.
+        .blocklist_type("KZGProof")
+        /*
+         * Re-build instructions
+         */
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .unwrap();
+
+    bindings
+        .write_to_file(bindings_out_path)
+        .expect("Failed to write bindings");
+    bindings
+        .write_to_file(snapshot_path)
+        .expect("Failed to write snapshot");
 }
