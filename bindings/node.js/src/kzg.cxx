7926b2d4c24818ce6a8c26c5d7af8816b50df974
matthewkeil <me@matthewkeil.com>
2023-03-27 10:35:37 -0400

    check fclose return value

--- bindings/node.js/src/kzg.cxx
+++ bindings/node.js/src/kzg.cxx
@@ -149,7 +149,10 @@ Napi::Value LoadTrustedSetup(const Napi::CallbackInfo& info) {
       return env.Undefined();
   }
   data->is_setup = true;
-  fclose(file_handle);
+  int close_err = fclose(file_handle);
+  if (close_err != 0) {
+      Napi::Error::New(env, "Error closing trusted setup file: " + file_path).ThrowAsJavaScriptException();
+  }
   return env.Undefined();
 }
 
