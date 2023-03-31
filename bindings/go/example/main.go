af86082cd4315277cb6838f374c2597c86069e01
Justin Traglia <jtraglia@pm.me>
2023-03-30 19:06:55 -0500

    Update Go bindings example

--- bindings/go/example/main.go
+++ bindings/go/example/main.go
@@ -10,15 +10,15 @@ import (
 var TrustedSetupPath = "../../../src/trusted_setup.txt"
 
 func main() {
-	ret := ckzg.LoadTrustedSetupFile(TrustedSetupPath)
-	if ret != ckzg.C_KZG_OK {
+	err := ckzg.LoadTrustedSetupFile(TrustedSetupPath)
+	if err != nil {
 		panic("failed to load trusted setup")
 	}
 	defer ckzg.FreeTrustedSetup()
 
 	blob := ckzg.Blob{1, 2, 3}
-	commitment, ret := ckzg.BlobToKZGCommitment(blob)
-	if ret != ckzg.C_KZG_OK {
+	commitment, err := ckzg.BlobToKZGCommitment(blob)
+	if err != nil {
 		panic("failed to get commitment for blob")
 	}
 	fmt.Println(hex.EncodeToString(commitment[:]))
