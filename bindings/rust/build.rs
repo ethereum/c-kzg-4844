a844da06ccac6d428d715bd339521833dcc37e75
Diva M <divma@protonmail.com>
2023-03-31 10:38:13 -0500

    delegate blst build to crate in preparation for windows bindings

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -15,21 +15,12 @@ fn move_file(src: &Path, dst: &Path) -> Result<(), String> {
 
 fn main() {
     let cargo_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let root_dir = cargo_dir.join("../../");
+    let root_dir = cargo_dir
+        .parent()
+        .expect("rust dir is nested")
+        .parent()
+        .expect("bindings dir is nested");
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    // Ensure libblst exists in `OUT_DIR`
-    // Assuming blst submodule exists
-    Command::new("make")
-        .current_dir(root_dir.join("src"))
-        .arg("blst")
-        .status()
-        .unwrap();
-    move_file(
-        root_dir.join("lib/libblst.a").as_path(),
-        out_dir.join("libblst.a").as_path(),
-    )
-    .unwrap();
 
     let field_elements_per_blob = if cfg!(feature = "minimal-spec") {
         MINIMAL_FIELD_ELEMENTS_PER_BLOB
@@ -41,7 +32,7 @@ fn main() {
 
     // Deleting any existing assembly and object files to ensure that compiling with a different
     // feature flag changes the final linked library file.
-    let obj_file = root_dir.join("src/c_kzg_4844.o");
+    let obj_file = root_dir.join("src").join("c_kzg_4844.o");
     if obj_file.exists() {
         std::fs::remove_file(obj_file).unwrap();
     }
@@ -63,7 +54,7 @@ fn main() {
         .status()
         .unwrap();
     move_file(
-        root_dir.join("src/libckzg.a").as_path(),
+        root_dir.join("src").join("libckzg.a").as_path(),
         out_dir.join("libckzg.a").as_path(),
     )
     .unwrap();
@@ -71,22 +62,31 @@ fn main() {
     println!("cargo:rustc-link-search={}", out_dir.display());
     println!("cargo:rustc-link-search={}", out_dir.display());
     println!("cargo:rustc-link-lib=static=ckzg");
+    // Tell cargo to search for the static blst exposed by the blst-bindings' crate.
     println!("cargo:rustc-link-lib=static=blst");
 
-    let bindings_out_path = cargo_dir.join("src/bindings/generated.rs");
+    let bindings_out_path = cargo_dir.join("src").join("bindings").join("generated.rs");
     let build_target = env::var("TARGET").unwrap();
     let snapshot_path = cargo_dir.join("snapshots").join(format!(
         "bindings_{build_target}_{field_elements_per_blob}.rs"
     ));
+
+    let header_file_path = root_dir.join("src").join("c_kzg_4844.h");
+    let header_file = header_file_path.to_str().expect("valid header file");
+
+    // Obtain the header files exposed by blst-bindings' crate.
+    let blst_headers_dir =
+        std::env::var_os("DEP_BLST_BINDINGS").expect("BLST exposes header files for bindings");
     make_bindings(
         field_elements_per_blob,
-        &root_dir,
+        header_file,
+        &blst_headers_dir.to_string_lossy(),
         bindings_out_path,
         snapshot_path,
     );
 
     // Cleanup
-    let obj_file = root_dir.join("src/c_kzg_4844.o");
+    let obj_file = root_dir.join("src").join("c_kzg_4844.o");
     if obj_file.exists() {
         std::fs::remove_file(obj_file).unwrap();
     }
@@ -94,7 +94,8 @@ fn main() {
 
 fn make_bindings<P>(
     field_elements_per_blob: usize,
-    root_dir: &Path,
+    header_path: &str,
+    blst_headers_dir: &str,
     bindings_out_path: P,
     snapshot_path: P,
 ) where
@@ -120,12 +121,6 @@ fn make_bindings<P>(
         }
     }
 
-    let header_file_path = root_dir.join("src/c_kzg_4844.h");
-    let header_file = header_file_path.to_str().expect("valid header file");
-
-    let inc_dir_path = root_dir.join("inc");
-    let inc_dir = inc_dir_path.to_str().expect("valid inc dir");
-
     let bindings = Builder::default()
         /*
          * Header definitions.
@@ -136,12 +131,12 @@ fn make_bindings<P>(
             "consts",
             &format!("#define FIELD_ELEMENTS_PER_BLOB {field_elements_per_blob}"),
         )
-        .header(header_file)
-        .clang_args([format!("-I{inc_dir}")])
+        .header(header_path)
+        .clang_args([format!("-I{blst_headers_dir}")])
         // Since this is not part of the header file, needs to be allowed explicitly.
         .allowlist_var("FIELD_ELEMENTS_PER_BLOB")
         // Get bindings only for the header file.
-        .allowlist_file(".*/c_kzg_4844.h")
+        .allowlist_file(".*c_kzg_4844.h")
         /*
          * Cleanup instructions.
          */
