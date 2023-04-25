2241d5df1e42dc55f4e82d97f2cbbb22f92bd924
Justin Traglia <jtraglia@pm.me>
2023-04-25 16:29:58 -0500

    Reorganize header file a little bit

--- bindings/go/header.h
+++ bindings/go/header.h
@@ -1,10 +1,14 @@
-#include <stdint.h>
-#include "server.c"
+/*
+ * Default to the mainnet preset.
+ */
+#ifndef FIELD_ELEMENTS_PER_BLOB
+#define FIELD_ELEMENTS_PER_BLOB 4096
+#endif
 
 /*
- * The necessary types from blst.h:
+ * The types from blst.h that C-KZG-4844 uses:
  */
-typedef uint8_t byte;
+typedef unsigned char byte;
 typedef long long unsigned int limb_t;
 typedef struct { byte b[256/8]; } blst_scalar;
 typedef struct { limb_t l[256/8/sizeof(limb_t)]; } blst_fr;
@@ -17,11 +21,5 @@ typedef struct { blst_fp x, y; } blst_p1_affine;
 typedef struct { blst_fp2 x, y, z; } blst_p2;
 typedef struct { blst_fp2 x, y; } blst_p2_affine;
 
-/*
- * Default to the mainnet preset.
- */
-#ifndef FIELD_ELEMENTS_PER_BLOB
-#define FIELD_ELEMENTS_PER_BLOB 4096
-#endif
-
+#include "server.c"
 #include "c_kzg_4844.c"
