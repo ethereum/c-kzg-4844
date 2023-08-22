47526cf3348701f83db601febef98733726255e3
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-08-22 14:39:54 -0400

    serialize and deserialize with prefix

--- bindings/rust/src/bindings/serde_helpers.rs
+++ bindings/rust/src/bindings/serde_helpers.rs
@@ -1,222 +1,152 @@
 //! Serde serialization and deserialization for the basic types in this crate.
-//!
-//! The implementations are modified from the `[T; N]` implementations in
-//! `serde` itself.
-//!
-//! Serialize impls from `serde`:
-//! <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/ser/impls.rs>
-//!
-//! Deserialize impls from `serde`:
-//! <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/de/impls.rs>
-use crate::{Blob, Bytes48, BYTES_PER_BLOB};
+use crate::{Blob, Bytes48};
 use serde::{
-    de::{SeqAccess, Visitor},
-    ser::SerializeTuple,
-    Deserialize, Deserializer, Serialize,
-    __private::de::InPlaceSeed,
+    Serializer, Deserialize, Deserializer, Serialize,
 };
-use std::{fmt, marker::PhantomData};
 
-/// Copied from serde:
-/// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/de/impls.rs#L1100-L1111>
-struct ArrayVisitor<A> {
-    marker: PhantomData<A>,
+/// Serialize a byte vec as a hex string with 0x prefix
+pub fn serialize_bytes<S, T>(x: T, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+    T: AsRef<[u8]>,
+{
+    s.serialize_str(&format!("0x{}", hex::encode(x.as_ref())))
 }
 
-struct ArrayInPlaceVisitor<'a, A: 'a>(&'a mut A);
-
-impl<A> ArrayVisitor<A> {
-    fn new() -> Self {
-        ArrayVisitor {
-            marker: PhantomData,
-        }
-    }
-}
-
-// === Blob ===
-
-// copied from serde:
-// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/ser/impls.rs#L143-L158>
 impl Serialize for Blob {
-    #[inline]
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut seq = serializer.serialize_tuple(BYTES_PER_BLOB)?;
-        for e in self.as_ref() {
-            seq.serialize_element(e)?;
-        }
-        seq.end()
+        where
+            S: Serializer {
+            serialize_bytes(self.bytes, serializer)
     }
 }
 
-// modified slightly from serde:
-// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/de/impls.rs#L1142-L1164>
-impl<'de> Visitor<'de> for ArrayVisitor<[u8; BYTES_PER_BLOB]> {
-    type Value = [u8; BYTES_PER_BLOB];
-
-    fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        formatter.write_str(format!("an array of length {}", BYTES_PER_BLOB).as_str())
-    }
-
-    #[inline]
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-    where
-        A: SeqAccess<'de>,
-    {
-        let mut arr = [0u8; BYTES_PER_BLOB];
-        for (i, elem) in arr.iter_mut().enumerate() {
-            match seq.next_element::<u8>()? {
-                Some(val) => *elem = val,
-                None => return Err(serde::de::Error::invalid_length(i, &self)),
-            }
-        }
-        Ok(arr)
-    }
-}
-
-// copied from serde:
-// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/de/impls.rs#L1166-L1193>
-impl<'de> Visitor<'de> for ArrayInPlaceVisitor<'_, [u8; BYTES_PER_BLOB]> {
-    type Value = ();
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str(format!("an array of length {}", BYTES_PER_BLOB).as_str())
-    }
-
-    #[inline]
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-    where
-        A: SeqAccess<'de>,
-    {
-        let mut fail_idx = None;
-        for (idx, dest) in self.0[..].iter_mut().enumerate() {
-            if seq.next_element_seed(InPlaceSeed(dest))?.is_none() {
-                fail_idx = Some(idx);
-                break;
-            }
-        }
-
-        if let Some(idx) = fail_idx {
-            return Err(serde::de::Error::invalid_length(idx, &self));
-        }
-
-        Ok(())
-    }
-}
-
-// copied from serde:
-// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/de/impls.rs#L1195-L1212>
 impl<'de> Deserialize<'de> for Blob {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
-        D: Deserializer<'de>,
-    {
-        let bytes = deserializer
-            .deserialize_tuple(BYTES_PER_BLOB, ArrayVisitor::<[u8; BYTES_PER_BLOB]>::new())?;
-        Ok(Blob { bytes })
-    }
+        D: Deserializer<'de> {
+        let value = String::deserialize(deserializer)?;
+        let bytes_res = match value.strip_prefix("0x") {
+            Some(value) => hex::decode(value),
+            None => hex::decode(&value),
+        };
 
-    fn deserialize_in_place<D>(deserializer: D, place: &mut Self) -> Result<(), D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_tuple(
-            BYTES_PER_BLOB,
-            ArrayInPlaceVisitor::<[u8; BYTES_PER_BLOB]>(place),
-        )
+        let bytes = bytes_res.map_err(|e| serde::de::Error::custom(e.to_string()))?;
+        Blob::from_bytes(bytes.as_slice()).map_err(|e| serde::de::Error::custom(format!("{:?}", e)))
     }
 }
 
-// === Bytes48 ===
-
-// copied from serde:
-// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/ser/impls.rs#L143-L158>
 impl Serialize for Bytes48 {
-    #[inline]
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut seq = serializer.serialize_tuple(48)?;
-        for e in self.as_ref() {
-            seq.serialize_element(e)?;
-        }
-        seq.end()
+        where
+            S: Serializer {
+            serialize_bytes(self.bytes, serializer)
     }
 }
 
-// modified slightly from serde:
-// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/de/impls.rs#L1142-L1164>
-impl<'de> Visitor<'de> for ArrayVisitor<[u8; 48]> {
-    type Value = [u8; 48];
-
-    fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        formatter.write_str(format!("an array of length {}", 48).as_str())
-    }
-
-    #[inline]
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-    where
-        A: SeqAccess<'de>,
-    {
-        let mut arr = [0u8; 48];
-        for (i, elem) in arr.iter_mut().enumerate() {
-            match seq.next_element::<u8>()? {
-                Some(val) => *elem = val,
-                None => return Err(serde::de::Error::invalid_length(i, &self)),
-            }
-        }
-        Ok(arr)
-    }
-}
-
-// copied from serde:
-// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/de/impls.rs#L1166-L1193>
-impl<'de> Visitor<'de> for ArrayInPlaceVisitor<'_, [u8; 48]> {
-    type Value = ();
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("an array of length 48")
-    }
-
-    #[inline]
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-    where
-        A: SeqAccess<'de>,
-    {
-        let mut fail_idx = None;
-        for (idx, dest) in self.0[..].iter_mut().enumerate() {
-            if seq.next_element_seed(InPlaceSeed(dest))?.is_none() {
-                fail_idx = Some(idx);
-                break;
-            }
-        }
-
-        if let Some(idx) = fail_idx {
-            return Err(serde::de::Error::invalid_length(idx, &self));
-        }
-
-        Ok(())
-    }
-}
-
-// copied from serde:
-// <https://github.com/serde-rs/serde/blob/7b548db91ed7da81a5c0ddbd6f6f21238aacfebe/serde/src/de/impls.rs#L1195-L1212>
 impl<'de> Deserialize<'de> for Bytes48 {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
-        D: Deserializer<'de>,
-    {
-        let bytes = deserializer.deserialize_tuple(48, ArrayVisitor::<[u8; 48]>::new())?;
-        Ok(Bytes48 { bytes })
+        D: Deserializer<'de> {
+        let value = String::deserialize(deserializer)?;
+        let bytes_res = match value.strip_prefix("0x") {
+            Some(value) => hex::decode(value),
+            None => hex::decode(&value),
+        };
+
+        let bytes = bytes_res.map_err(|e| serde::de::Error::custom(e.to_string()))?;
+        Bytes48::from_bytes(bytes.as_slice()).map_err(|e| serde::de::Error::custom(format!("{:?}", e)))
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+    use rand::{rngs::ThreadRng, Rng};
+    use std::path::PathBuf;
+
+    fn generate_random_blob(rng: &mut ThreadRng) -> Blob {
+        let mut arr = [0u8; BYTES_PER_BLOB];
+        rng.fill(&mut arr[..]);
+        // Ensure that the blob is canonical by ensuring that
+        // each field element contained in the blob is < BLS_MODULUS
+        for i in 0..FIELD_ELEMENTS_PER_BLOB {
+            arr[i * BYTES_PER_FIELD_ELEMENT] = 0;
+        }
+        arr.into()
     }
 
-    fn deserialize_in_place<D>(deserializer: D, place: &mut Self) -> Result<(), D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_tuple(48, ArrayInPlaceVisitor::<[u8; 48]>(place))
+    #[test]
+    fn test_serialize_roundtrip() {
+        // load setup so we can create commitments and blobs
+        let trusted_setup_file = PathBuf::from("../../src/trusted_setup.txt");
+        assert!(trusted_setup_file.exists());
+        let kzg_settings = KZGSettings::load_trusted_setup_file(trusted_setup_file).unwrap();
+
+        // generate blob, commitment, proof
+        let mut rng = rand::thread_rng();
+        let blob = generate_random_blob(&mut rng);
+        let commitment = KZGCommitment::blob_to_kzg_commitment(blob.clone(), &kzg_settings).unwrap();
+        let proof = KZGProof::compute_blob_kzg_proof(blob.clone(), commitment.to_bytes(), &kzg_settings).unwrap();
+
+        // check blob serialization
+        let blob_serialized = serde_json::to_string(&blob).unwrap();
+        let blob_deserialized: Blob = serde_json::from_str(&blob_serialized).unwrap();
+        assert_eq!(blob, blob_deserialized);
+
+        // check commitment serialization
+        let commitment_serialized = serde_json::to_string(&commitment.to_bytes()).unwrap();
+        let commitment_deserialized: Bytes48 = serde_json::from_str(&commitment_serialized).unwrap();
+        assert_eq!(commitment.to_bytes(), commitment_deserialized);
+
+        // check proof serialization
+        let proof_serialized = serde_json::to_string(&proof.to_bytes()).unwrap();
+        let proof_deserialized: Bytes48 = serde_json::from_str(&proof_serialized).unwrap();
+        assert_eq!(proof.to_bytes(), proof_deserialized);
+    }
+
+    #[test]
+    fn test_serialize_blob_with_prefix() {
+        // generate blob
+        let mut rng = rand::thread_rng();
+        let blob = generate_random_blob(&mut rng);
+
+        // check blob serialization
+        let blob_serialized = serde_json::to_string(&blob).unwrap();
+
+        // check that this begins with a quote and 0x
+        let mut chars = blob_serialized.chars();
+        assert_eq!(chars.next().unwrap(), '"');
+        assert_eq!(chars.next().unwrap(), '0');
+        assert_eq!(chars.next().unwrap(), 'x');
+
+        // check that it ends with a quote (sanity check)
+        assert_eq!(chars.last().unwrap(), '"');
+    }
+
+    #[test]
+    fn test_serialize_bytes_48_with_prefix() {
+        // load setup so we can create a commitments
+        let trusted_setup_file = PathBuf::from("../../src/trusted_setup.txt");
+        assert!(trusted_setup_file.exists());
+        let kzg_settings = KZGSettings::load_trusted_setup_file(trusted_setup_file).unwrap();
+
+        // generate blob just to calculate a commitment
+        let mut rng = rand::thread_rng();
+        let blob = generate_random_blob(&mut rng);
+        let commitment = KZGCommitment::blob_to_kzg_commitment(blob.clone(), &kzg_settings).unwrap();
+
+        // check blob serialization
+        let blob_serialized = serde_json::to_string(&commitment.to_bytes()).unwrap();
+
+        // check that this begins with a quote and 0x
+        let mut chars = blob_serialized.chars();
+        assert_eq!(chars.next().unwrap(), '"');
+        assert_eq!(chars.next().unwrap(), '0');
+        assert_eq!(chars.next().unwrap(), 'x');
+
+        // check that it ends with a quote (sanity check)
+        assert_eq!(chars.last().unwrap(), '"');
     }
 }
