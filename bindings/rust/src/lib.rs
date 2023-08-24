4c0d477c0f21987e21201458d65f9d64fb889b2f
Michael Sproul <michael@sigmaprime.io>
2023-08-25 06:13:39 +1000

    Use blst crate for Rust blst dep (#351)

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -1,3 +1,8 @@
+// This `extern crate` invocation tells `rustc` that we actually need the symbols from `blst`.
+// Without it, the compiler won't link to `blst` when compiling this crate.
+// See: https://kornel.ski/rust-sys-crate#linking
+extern crate blst;
+
 mod bindings;
 
 // Expose relevant types with idiomatic names.
@@ -7,8 +12,8 @@ pub use bindings::{
 };
 // Expose the constants.
 pub use bindings::{
-    BYTES_PER_BLOB, BYTES_PER_COMMITMENT, BYTES_PER_FIELD_ELEMENT, BYTES_PER_PROOF,
-    FIELD_ELEMENTS_PER_BLOB, BYTES_PER_G1_POINT, BYTES_PER_G2_POINT
+    BYTES_PER_BLOB, BYTES_PER_COMMITMENT, BYTES_PER_FIELD_ELEMENT, BYTES_PER_G1_POINT,
+    BYTES_PER_G2_POINT, BYTES_PER_PROOF, FIELD_ELEMENTS_PER_BLOB,
 };
 // Expose the remaining relevant types.
 pub use bindings::{Blob, Bytes32, Bytes48, Error};
