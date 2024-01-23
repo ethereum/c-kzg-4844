68ee8e2a5339d0169591d3f476b850191215f122
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2024-01-23 12:16:50 -0700

    Add pub methods for cell and export das items (#386)

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(new_uninit)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[macro_use]
@@ -19,7 +18,8 @@ pub use bindings::{
 // Expose the constants.
 pub use bindings::{
     BYTES_PER_BLOB, BYTES_PER_COMMITMENT, BYTES_PER_FIELD_ELEMENT, BYTES_PER_G1_POINT,
-    BYTES_PER_G2_POINT, BYTES_PER_PROOF, FIELD_ELEMENTS_PER_BLOB,
+    BYTES_PER_G2_POINT, BYTES_PER_PROOF, CELLS_PER_BLOB, DATA_POINTS_PER_BLOB,
+    FIELD_ELEMENTS_PER_BLOB, FIELD_ELEMENTS_PER_CELL,
 };
 // Expose the remaining relevant types.
-pub use bindings::{Blob, Bytes32, Bytes48, Error};
+pub use bindings::{Blob, Bytes32, Bytes48, Cell, Error};
