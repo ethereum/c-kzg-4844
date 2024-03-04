1cab6aa690440c3cec45c8c4034a13c6ce9ddd5a
Justin Traglia <jtraglia@pm.me>
2024-03-04 14:07:57 -0600

    Add more information in setuptools.setup()

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, Extension
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 
 def main():
@@ -6,7 +10,12 @@ def main():
         name="ckzg",
         version="0.4.3",
         author="Ethereum Foundation",
+        author_email="security@ethereum.org",
+        url="https://github.com/ethereum/c-kzg-4844",
         description="Python bindings for C-KZG-4844",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="Apache-2.0",
         ext_modules=[
             Extension(
                 "ckzg",
