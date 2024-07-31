6e58753c1cf67e39feb7b8f144a991a132f3f5d8
Justin Traglia <jtraglia@pm.me>
2024-07-31 09:11:08 -0500

    Remove setup functions from eip4844.h

--- src/eip4844.h
+++ src/eip4844.h
@@ -54,21 +54,6 @@ typedef struct {
 // Public Functions
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-C_KZG_RET load_trusted_setup(
-    KZGSettings *out,
-    const uint8_t *g1_monomial_bytes,
-    size_t num_g1_monomial_bytes,
-    const uint8_t *g1_lagrange_bytes,
-    size_t num_g1_lagrange_bytes,
-    const uint8_t *g2_monomial_bytes,
-    size_t num_g2_monomial_bytes,
-    size_t precompute
-);
-
-C_KZG_RET load_trusted_setup_file(KZGSettings *out, FILE *in, size_t precompute);
-
-void free_trusted_setup(KZGSettings *s);
-
 C_KZG_RET blob_to_kzg_commitment(KZGCommitment *out, const Blob *blob, const KZGSettings *s);
 
 C_KZG_RET compute_kzg_proof(
