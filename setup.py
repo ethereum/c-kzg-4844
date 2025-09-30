58ea1eef092c2f1b30108aef96e024cb847ba660
Justin Traglia <jtraglia@pm.me>
2025-09-30 11:59:27 -0500

    Bump versions to v2.1.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def main():
 
     setup(
         name="ckzg",
-        version="2.1.4",
+        version="2.1.5",
         author="Ethereum Foundation",
         author_email="security@ethereum.org",
         url="https://github.com/ethereum/c-kzg-4844",
